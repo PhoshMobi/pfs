d2514dd5363dd3781e8052f0aec8f3a54b9f8d2f
Guido Günther <agx@sigxcpu.org>
2025-11-19 16:41:29 +0100

    pfs-open: Split out opening dirs
    This will allow us to handle open
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>

--- src/examples/open/pfs_open_application.rs
+++ src/examples/open/pfs_open_application.rs
@@ -37,46 +37,8 @@ mod imp {
         fn activate(&self) {
             let application = self.obj();
 
-            *self.hold_guard.borrow_mut() = Some(application.hold());
-
             let home = glib::home_dir();
-            let file_selector = glib::Object::builder::<FileSelector>()
-                .property("accept_label", "Done")
-                .property("title", "Select a File")
-                .property("current-folder", gio::File::for_path(&home))
-                .build();
-
-            file_selector.connect_closure(
-                "done",
-                false,
-                glib::closure_local!(
-                    #[weak(rename_to = this)]
-                    self,
-                    move |selector: FileSelector, success: bool| {
-                        glib::g_debug!(LOG_DOMAIN, "File dialog done, result: {success:#?}");
-                        let selected = selector.selected();
-
-                        if success {
-                            let uris = match selected {
-                                None => vec!["".to_string()],
-                                Some(vec) => vec,
-                            };
-                            glib::g_message!(LOG_DOMAIN, "Opening {uris:#?}");
-
-                            Command::new("gio")
-                                .arg("open")
-                                .arg(&uris[0])
-                                .spawn()
-                                .expect("Failed to open {uris[0]:?}");
-                        }
-                        // Drop the application ref count
-                        this.hold_guard.replace(None);
-                    }
-                ),
-            );
-
-            file_selector.set_mode(FileSelectorMode::OpenFile);
-            file_selector.present();
+            application.open_directory(&gio::File::for_path(&home));
         }
     }
 
@@ -91,10 +53,55 @@ glib::wrapper! {
 }
 
 impl PfsOpenApplication {
-    pub fn new(application_id: &str, flags: &gio::ApplicationFlags) -> Self {
+    pub fn new(application_id: &str) -> Self {
         glib::Object::builder()
             .property("application-id", application_id)
-            .property("flags", flags)
+            .property("flags", &gio::ApplicationFlags::HANDLES_OPEN)
             .build()
+    }
+
+    fn open_directory(&self, file: &gio::File) {
+        let uri = file.uri();
+        glib::g_message!(LOG_DOMAIN,"Opening {uri:#?}");
+
+        *self.imp().hold_guard.borrow_mut() = Some(self.hold());
+
+        let file_selector = glib::Object::builder::<FileSelector>()
+            .property("accept_label", "Done")
+            .property("title", "Select a File")
+            .property("current-folder", file)
+            .build();
+
+        file_selector.connect_closure(
+            "done",
+            false,
+            glib::closure_local!(
+                #[weak(rename_to = this)]
+                self,
+                move |selector: FileSelector, success: bool| {
+                    glib::g_debug!(LOG_DOMAIN, "File dialog done, result: {success:#?}");
+                    let selected = selector.selected();
+
+                    if success {
+                        let uris = match selected {
+                            None => vec!["".to_string()],
+                            Some(vec) => vec,
+                        };
+                        glib::g_message!(LOG_DOMAIN, "Opening {uris:#?}");
+
+                        Command::new("gio")
+                            .arg("open")
+                            .arg(&uris[0])
+                            .spawn()
+                            .expect("Failed to open {uris[0]:?}");
+                    }
+                    // Drop the application ref count
+                    this.imp().hold_guard.replace(None);
+                }
+            ),
+        );
+
+        file_selector.set_mode(FileSelectorMode::OpenFile);
+        file_selector.present();
     }
 }
