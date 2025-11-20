9db53541c70d2616524a9cacb6964bfe6077fc87
Guido Günther <agx@sigxcpu.org>
2025-11-20 19:43:41 +0100

    pfs-open: Add org.freedesktop.FileManager1 interface
    See examples/gio_dbus_register_object/main.rs in gtk-rs
    
    ```
    $ busctl --user call org.freedesktop.FileManager1 /org/freedesktop/FileManager1 org.freedesktop.FileManager1 ShowFolders ass 2 file:///tmp file:///etc asdf
    
    $ busctl --user call org.freedesktop.FileManager1 /org/freedesktop/FileManager1 org.freedesktop.FileManager1 ShowItems ass 1 file:///tmp asdf
    ```
    
    We lack the `ShowItemProperties` method but that'll be
    a completely separate window.
    
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/pfs/-/merge_requests/36>

--- src/examples/open/pfs_open_application.rs
+++ src/examples/open/pfs_open_application.rs
@@ -8,6 +8,7 @@
 
 use adw::prelude::*;
 use adw::subclass::prelude::*;
+use glib_macros::clone;
 use gtk::{gio, glib};
 use std::cell::{Cell, RefCell};
 use std::process::Command;
@@ -16,13 +17,76 @@ use pfs::file_selector::{FileSelector, FileSelectorMode};
 
 use crate::config::LOG_DOMAIN;
 
+const FILE_MANAGER1_NAME: &str = "org.freedesktop.FileManager1";
+const FILE_MANAGER1_XML: &str = r#"
+<node>
+  <interface name='org.freedesktop.FileManager1'>
+     <method name='ShowFolders'>
+      <arg type='as' name='URIs' direction='in'/>
+      <arg type='s' name='StartupId' direction='in'/>
+    </method>
+    <method name='ShowItems'>
+      <arg type='as' name='URIs' direction='in'/>
+      <arg type='s' name='StartupId' direction='in'/>
+    </method>
+<!-- Not yet implemented
+    <method name='ShowItemProperties'>
+      <arg type='as' name='URIs' direction='in'/>
+      <arg type='s' name='StartupId' direction='in'/>
+    </method>
+-->
+  </interface>
+</node>
+"#;
+
+#[derive(Debug, glib::Variant)]
+struct ShowFolders {
+    uris: Vec<String>,
+    _startup_id: String,
+}
+
+#[derive(Debug, glib::Variant)]
+struct ShowItems {
+    uris: Vec<String>,
+    _startup_id: String,
+}
+
+#[derive(Debug)]
+enum FileManager1 {
+    ShowFolders(ShowFolders),
+    ShowItems(ShowItems),
+}
+
 mod imp {
     use super::*;
+
+    impl DBusMethodCall for FileManager1 {
+        fn parse_call(
+            _obj_path: &str,
+            _interface: Option<&str>,
+            method: &str,
+            params: glib::Variant,
+        ) -> Result<Self, glib::Error> {
+            match method {
+                "ShowFolders" => Ok(params.get::<ShowFolders>().map(Self::ShowFolders)),
+                "ShowItems" => Ok(params.get::<ShowItems>().map(Self::ShowItems)),
+                _ => Err(glib::Error::new(
+                    gio::DBusError::UnknownMethod,
+                    "No such method",
+                )),
+            }
+            .and_then(|p| {
+                p.ok_or_else(|| glib::Error::new(gio::DBusError::InvalidArgs, "Invalid parameters"))
+            })
+        }
+    }
 
     #[derive(Debug, Default)]
     pub struct PfsOpenApplication {
         pub hold_guard: RefCell<Option<gio::ApplicationHoldGuard>>,
         pub hold_count: Cell<u32>,
+        registration_id: RefCell<Option<gio::RegistrationId>>,
+        owner_id: RefCell<Option<gio::OwnerId>>,
     }
 
     #[glib::object_subclass]
@@ -53,6 +117,56 @@ mod imp {
                 self.obj().open_directory(file);
             }
         }
+
+        fn dbus_register(
+            &self,
+            connection: &gio::DBusConnection,
+            object_path: &str,
+        ) -> Result<(), glib::Error> {
+            self.parent_dbus_register(connection, object_path)?;
+
+            if let Ok(id) = self.obj().register_object(connection) {
+                glib::g_debug!(LOG_DOMAIN, "Exported FileManager1 DBus interface");
+                self.registration_id.replace(Some(id));
+            } else {
+                glib::g_warning!(LOG_DOMAIN, "Failed to export FileManager1 DBus interface");
+            }
+
+            let id = gio::bus_own_name_on_connection(
+                connection,
+                FILE_MANAGER1_NAME,
+                gio::BusNameOwnerFlags::REPLACE | gio::BusNameOwnerFlags::ALLOW_REPLACEMENT,
+                |_connection, name| {
+                    glib::g_debug!(LOG_DOMAIN, "Owned {name} DBus name");
+                },
+                clone!(
+                    #[weak(rename_to = this)]
+                    self,
+                    move |_connection, name| {
+                        glib::g_warning!(LOG_DOMAIN, "Lost {name} DBus name");
+                        this.owner_id.replace(None);
+                    }
+                ),
+            );
+
+            self.owner_id.replace(Some(id));
+            Ok(())
+        }
+
+        fn dbus_unregister(&self, connection: &gio::DBusConnection, object_path: &str) {
+            self.parent_dbus_unregister(connection, object_path);
+            if let Some(id) = self.registration_id.take() {
+                if connection.unregister_object(id).is_ok() {
+                    glib::g_debug!(LOG_DOMAIN, "Unregistered object");
+                } else {
+                    glib::g_warning!(LOG_DOMAIN, "Could not unregister object");
+                }
+            }
+
+            if let Some(owner_id) = self.owner_id.replace(None) {
+                gio::bus_unown_name(owner_id);
+            }
+        }
     }
 
     impl GtkApplicationImpl for PfsOpenApplication {}
@@ -73,11 +187,11 @@ impl PfsOpenApplication {
             .build()
     }
 
-    fn open_directory(&self, file: &gio::File) {
+    fn open_directory(&self, dir: &gio::File) -> FileSelector {
         let imp = self.imp();
-        let uri = file.uri();
+        let uri = dir.uri();
 
-        glib::g_message!(LOG_DOMAIN, "Opening {uri:#?}");
+        glib::g_message!(LOG_DOMAIN, "Opening {uri}");
 
         if imp.hold_count.replace(imp.hold_count.get() + 1) == 0 {
             *self.imp().hold_guard.borrow_mut() = Some(self.hold());
@@ -85,8 +199,8 @@ impl PfsOpenApplication {
 
         let file_selector = glib::Object::builder::<FileSelector>()
             .property("accept_label", gettextrs::gettext("Open"))
-            .property("title", "Select a File")
-            .property("current-folder", file)
+            .property("title", gettextrs::gettext("Select a File"))
+            .property("current-folder", dir)
             .build();
 
         file_selector.connect_closure(
@@ -126,5 +240,57 @@ impl PfsOpenApplication {
 
         file_selector.set_mode(FileSelectorMode::OpenFile);
         file_selector.present();
+
+        file_selector
+    }
+
+    fn select_item(&self, file: &gio::File) {
+        if let Some(parent) = file.parent() {
+            let file_selector = self.open_directory(&parent);
+            file_selector.select_item(file);
+        }
+    }
+
+    fn register_object(
+        &self,
+        connection: &gio::DBusConnection,
+    ) -> Result<gio::RegistrationId, glib::Error> {
+        let file_manager1 = gio::DBusNodeInfo::for_xml(FILE_MANAGER1_XML)
+            .ok()
+            .and_then(|e| e.lookup_interface("org.freedesktop.FileManager1"))
+            .expect("FileManagaer1 interface");
+
+        connection
+            .register_object("/org/freedesktop/FileManager1", &file_manager1)
+            .typed_method_call::<FileManager1>()
+            .invoke_and_return_future_local(glib::clone!(
+                #[weak_allow_none(rename_to = this)]
+                self.imp(),
+                move |_, sender, call| {
+                    glib::g_message!(LOG_DOMAIN, "Method call from {sender:?}");
+                    let app = this.clone();
+                    async move {
+                        match call {
+                            FileManager1::ShowFolders(ShowFolders { uris, _startup_id }) => {
+                                if let Some(app) = app {
+                                    for uri in &uris {
+                                        app.obj().open_directory(&gio::File::for_uri(uri));
+                                    }
+                                }
+                                Ok(None)
+                            }
+                            FileManager1::ShowItems(ShowItems { uris, _startup_id }) => {
+                                if let Some(app) = app {
+                                    for uri in &uris {
+                                        app.obj().select_item(&gio::File::for_uri(uri));
+                                    }
+                                }
+                                Ok(None)
+                            }
+                        }
+                    }
+                }
+            ))
+            .build()
     }
 }
