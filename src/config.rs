0d0a1be78c2e697e849ffa5892d05a38e409eda9
Guido Günther <agx@sigxcpu.org>
2026-02-09 13:29:28 +0100

    treewide: Release 0.0.7
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/pfs/-/merge_requests/46>

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
 #[allow(dead_code)]
-pub static VERSION: &str = "0.0.5";
+pub static VERSION: &str = "0.0.7";
 pub static GETTEXT_PACKAGE: &str = "pfs";
 pub static LOG_DOMAIN: &str = "pfs";
 pub static LOCALEDIR: &str = "/usr/local/share/locale";
