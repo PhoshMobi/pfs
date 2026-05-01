459bd1f732bd743774afc4cd0c903a00029f463a
Guido Günther <agx@sigxcpu.org>
2026-05-01 15:11:16 +0200

    tests: Use file selector builder
    Signed-off-by: Guido Günther <agx@sigxcpu.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/pfs/-/merge_requests/60>

--- tests/api.rs
+++ tests/api.rs
@@ -1,19 +1,21 @@
+use gtk::gio;
 use gtk::prelude::*;
-use gtk::{gio, glib};
+
+use pfs::file_selector::FileSelectorBuilder;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new() {
+    fn test_new_file_selector() {
         assert_eq!(gtk::init().is_ok(), true);
         pfs::init::init();
 
-        let file_selector = glib::Object::builder::<pfs::file_selector::FileSelector>()
-            .property("accept_label", "Done")
-            .property("title", "Select a File")
-            .property("current-folder", gio::File::for_path("/tmp"))
+        let file_selector = FileSelectorBuilder::new()
+            .accept_label("Done")
+            .title("Select a File")
+            .current_folder(gio::File::for_path("/tmp"))
             .build();
 
         assert_eq!(file_selector.selected_choices().is_none(), true);
